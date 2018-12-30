47633565c77567ba0b0ce8d8f7948e0eacc4107e
Åsmund Stavdahl <smund3@gmail.com>
2018-12-30 19:55:59 +0100

    move templates, databases

--- src/AppKernel.php
+++ src/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
@@ -46,6 +45,6 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load($this->getProjectDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
 }
